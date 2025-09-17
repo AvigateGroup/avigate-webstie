811726e64d4aa755a0549b1dc4e3a51c64a1c047
Joel Emmanuel <joel.emmanuel@compasso.com.br>
2025-09-17 17:46:37 +0100

    build: uninstall tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import LandingPage from './components/LandingPage'
-import './App.css'
 
 function App() {
-  return (
-    <>
-      <LandingPage />
-    </>
-  )
+  return <LandingPage />
 }
 
 export default App
