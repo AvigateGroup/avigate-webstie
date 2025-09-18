7e4662e1f3a4eff8e1041682036f752377acf8b6
Joel Emmanuel <joel.emmanuel@compasso.com.br>
2025-09-18 08:16:57 +0100

    This adds state management for page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import LandingPage from './components/LandingPage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import PrivacyPolicy from './components/PrivacyPolicy';
 
 function App() {
-  return <LandingPage />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        {/* Fallback route */}
+        <Route path="*" element={<LandingPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
