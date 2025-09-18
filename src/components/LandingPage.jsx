af45ba8c435694820bfc9f6a440fe7f202436858
Joel Emmanuel <joel.emmanuel@compasso.com.br>
2025-09-18 09:17:21 +0100

    handles page refresh and direct URL access scenarios

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Navigation from './Navigation';
 import HeroSection from './HeroSection';
 import FeaturesSection from './FeaturesSection';
@@ -7,6 +8,10 @@ import CTASection from './CTASection';
 import Footer from './Footer';
 
 const LandingPage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="min-h-screen bg-white">
       <Navigation />
